f69009068e473853512f592c823219af3b178a95
Leonardo Huerfano <huerfano.jerson@gmail.com>
2021-07-17 14:49:02 -0500

    Implemented injector application

--- include/hardwaredef.h
+++ include/hardwaredef.h
@@ -1,6 +1,11 @@
 #ifndef HARDWAREDEF_H_
 #define HARDWAREDEF_H_
 
-#define IN_START 0
+#define IN_START        0
+#define IN_MANUAL       1
+#define IN_SAUTO        2
+#define IN_AUTO         3
+
+#define OUT_PUMP        20
 
 #endif
--- stampi-firmware.ino
+++ stampi-firmware.ino
@@ -12,10 +12,20 @@
 #include <Timer.h>
 #include <Transition.h>
 
+#include "include/microhal.h"
+
 void setup() {
 
 }
 
 void loop() {
     
+}
+
+bool hal_digital_read(const int & pin) {
+    return digitalRead(pin) == HIGH;
+}
+
+void hal_digital_write(const int & pin, const bool & value) {
+    digitalWrite(pin, value ? HIGH : LOW);
 }
